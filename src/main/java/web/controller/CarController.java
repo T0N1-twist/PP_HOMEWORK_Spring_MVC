360113b24c22596a4b817d56983bea18ff3c45d1
Tony Twist <tonitvist@gmail.com>
2026-03-14 23:38:36 +0300

    Fix minor changes and mentors comments

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,22 +1,33 @@
 package web.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import web.service.CarService;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.service.CarServiceImpl;
 
 @Controller
-@RequestMapping ("/cars")
+@RequestMapping("/cars")
 public class CarController {
-   @Autowired
-    private CarService carService;
 
-@GetMapping ("/")
-    public String showAllCars(ModelMap model) {
-    model.addAttribute("cars", carService.getAllCars());
-    return "cars";
-}
+    private final CarServiceImpl carServiceImpl;
+
+    public CarController(CarServiceImpl carServiceImpl) {
+        this.carServiceImpl = carServiceImpl;
+    }
+
+    @GetMapping
+    public String showAllCars(@RequestParam(required = false) Integer count,
+                          ModelMap model) {
+
+        if (count == null) {
+            model.addAttribute("cars", carServiceImpl.getAllCars());
+        } else {
+            model.addAttribute("cars", carServiceImpl.getCarsLimited(count));
+        }
+
+        return "cars";
+    }
 
 }
--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,33 +1,12 @@
 package web.service;
 
-import org.springframework.stereotype.Service;
 import web.model.Car;
 
-import java.util.ArrayList;
 import java.util.List;
-@Service
-public class CarService {
-    private List<Car> cars = new ArrayList<>();
 
-    public CarService() {
-        cars.add(new Car("BMW", "red", 2010));
-        cars.add(new Car("Mazda", "yello", 2023));
-        cars.add(new Car("Mercedes", "black", 1999));
-        cars.add(new Car("BMW", "blue", 2022));
-    }
+public interface CarService {
 
-    public List<Car> getCarsLimited(int count) {
-        int limit = Math.min(count, cars.size());
-            if (limit <= 0) {
-                limit = cars.size();
-            }
-        return new ArrayList<>(cars.subList(0, limit));
-    }
+    public List<Car> getCarsLimited(int count);
 
-
-    public List<Car> getAllCars() {
-        return new ArrayList<>(cars);
-    }
-
-
+    public List<Car>  getAllCars();
 }
